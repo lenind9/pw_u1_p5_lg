996d9adc2b1ae1661412ab90c43d2c3e18f4225f
“lenind9” <lenindgc@hotmail.com>
2023-12-09 10:37:15 -0500

    Vue methods y data

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ console.log("elementos Vue");
 console.log(Vue);
 
 const app = Vue.createApp({
+    // Options API
     //template:`
     //<h1>Hola mundo</h1>
     //<p>Desde app.js</p>
@@ -9,5 +10,26 @@ const app = Vue.createApp({
     //<p>{{[1, 2, 3, 4, 5, 6, 7, 8]}}</p>
     //<p>{{false?'Verdadero': 'Falso'}}</p>
     //`
+    methods:{
+        cambiarMensaje() {
+            console.log("Se esta cambiando el  mensaje")
+            console.log(this.mensaje)
+            this.mensaje='Valor cambiado'
+        },
+        cambiarNumero() {
+            console.log(this.valor)
+            this.valor++;
+        }
+    },
+    watch:{
+
+    },
+    data(){
+        return {
+            mensaje:'Hola mundo desde Vue.JS',
+            valor:100
+        }
+    }
 });
+
 app.mount('#miApp')
